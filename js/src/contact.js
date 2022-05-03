fa4f27d7af31ba0183096a3a3834f1bb0451e142
James Pederson <james@jpederson.com>
2022-05-03 17:39:04 -0500

    Update javascript to include the correct field name for account.

--- js/src/contact.js
+++ js/src/contact.js
@@ -21,7 +21,7 @@ var contact_submit = function( form ) {
 			name: $( form ).find( "input[name=name]" ).val(),
 			email: $( form ).find( "input[name=email]" ).val(),
 			phone: $( form ).find( "input[name=phone]" ).val(),
-			donate: $( form ).find( "select[name=donate]" ).val()
+			account: $( form ).find( "select[name=account]" ).val()
 		},
 		url_params = $.param( field ),
 		errors = [],
